5ef85b123386f4597893c67457bf2e5db44f0f61
beteab548 <beteabbaynessagne548@gmail.com>
2024-08-18 00:07:31 -0700

    3rd

---
+++ components/gameBoard.jsx
@@ -0,0 +1,22 @@
+const gameBoadrdArray = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+export default function GameBoard() {
+  return (
+    <ol id="game-board">
+      {gameBoadrdArray.map((row, rowIndex) => {
+        return <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => {
+              return <li key={colIndex}>
+                <button>{playerSymbol}</button>
+              </li>;
+            })}
+          </ol>
+        </li>;
+      })}
+    </ol>
+  );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import GameBoard from "../components/gameBoard";
 import Player from "../components/players";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
           <Player initialName="player1" symbol="x" />
           <Player initialName="player2" symbol="0" />
         </ul>
+        <GameBoard />
       </div>
     </main>
   );
